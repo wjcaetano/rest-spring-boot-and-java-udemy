cc6e969ba7022e4fb01f11daf6e003b51f1d7bef
wjcaetano <caetano.willian@gmail.com>
2023-01-03 17:37:15 -0300

    Create a first endpoint: Greeting

---
+++ src/main/java/com/wcaetano/restspringbootandjavaudemy/Greeting.java
@@ -0,0 +1,20 @@
+package com.wcaetano.restspringbootandjavaudemy;
+
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
